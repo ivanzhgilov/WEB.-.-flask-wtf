55e2603ac1038bdfcc1b8e4eef75c0d790cab06c
Ivanz <ivanz1701@mail.ru>
2023-03-30 06:54:27 +0500

    task 2

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ def index():
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    title = "Тренировочный центр"
+    if "инженер" in prof or "строитель" in prof:
+        professional_orientation = "Инженерные тренажеры"
+    else:
+        professional_orientation = "Научные симуляторы"
+    return render_template('training.html', title=title, professional_orientation=professional_orientation)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
