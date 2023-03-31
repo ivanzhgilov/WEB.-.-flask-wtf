238332af4c5eb8439e696718819aaa80f188379d
Ivanz <ivanz1701@mail.ru>
2023-03-31 23:51:36 +0500

    task 7

--- main.py
+++ main.py
@@ -63,5 +63,9 @@ def distribution():
     return render_template('distribution.html', astronauts=astronauts)
 
 
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('table.html', age=int(age), sex=sex)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
